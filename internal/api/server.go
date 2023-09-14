de93d30f276fab00b7c12f578175fcc875b915b9
Vanv1k <ivanspecial5@gmail.com>
2023-09-14 21:41:50 +0300

    added main page

--- internal/api/server.go
+++ internal/api/server.go
@@ -11,16 +11,33 @@ func StartServer() {
 	log.Println("Server start up")
 	
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context){
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK,  gin.H{
 			"message": "hello",
 		})
 	})
+
+	r.LoadHTMLGlob("templates/*")
+	
+	r.GET("/home", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
+	})
+	
+	r.Static("/styles", "./resources/styles")
+	r.Static("/js", "./resources/js")
+	r.Static("/img", "./resources/img")
+	r.Static("/hacker", "./resources")
+
+
 	r.Run()
 
 	log.Println("Server down")
